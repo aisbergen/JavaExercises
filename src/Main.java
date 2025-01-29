3a3003a871757d71e807f8b76ef7701f55c95bf1
Aisulu <aisulukakharovaa@gmail.com>
2025-01-29 18:02:28 +0000

    Fibonacci second method

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
 public class Main {
+    static int fibonacci(int n){
+        if (n >= 1) return n;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
     public static void main(String[] args){
-        Fibonacci ex = new Fibonacci();
-        ex.calculate(5);
+        //Fibonacci.calculate(4);
+        for(int i=2; i<=10; i++){
+            int fib = fibonacci(i);
+            System.out.println(i + fib);
+        }
+
 
     }
 }
 
-class Fibonacci{
-        public void calculate(int n){
+/*class Fibonacci{
+        public static void calculate(int n){
             //Scanner myObj = new Scanner(System.in);
             int F1 = 1;
             int F0 = 0;
             //int n = myObj.nextInt();
-            if (n == 0) return;
-            if(n==1) return;
+            if (n == 0) return 0;
+            if(n==1) return 1;
             for(int i=2; i<=n; i++){
                 int next = F1+F0;    // Add previous two numbers
                 F0 = F1;            // Move F0 to next position
@@ -21,5 +29,5 @@ class Fibonacci{
             }
             System.out.println(F1);
         }
-}
+}*/
 
