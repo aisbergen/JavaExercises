f96318a484e8ed1972a51867c232b241c89163e3
Aisulu <aisulukakharovaa@gmail.com>
2025-01-29 17:17:17 +0000

    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Fibonacci ex = new Fibonacci();
+        ex.calculate(5);
 
     }
 }
+
+class Fibonacci{
+        public void calculate(int n){
+            //Scanner myObj = new Scanner(System.in);
+            int F1 = 1;
+            int F0 = 0;
+            //int n = myObj.nextInt();
+            if (n == 0) return;
+            if(n==1) return;
+            for(int i=2; i<=n; i++){
+                int next = F1+F0;    // Add previous two numbers
+                F0 = F1;            // Move F0 to next position
+                F1 = next;          // Move F1 to next Fibonacci number
+            }
+            System.out.println(F1);
+        }
+}
+
