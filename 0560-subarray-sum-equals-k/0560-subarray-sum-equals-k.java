2f913f6fc1aca84211ec4f6ce2f3d004efb39e8f
Aisulu <102958404+aisbergen@users.noreply.github.com>
2025-06-08 12:22:51 +0100

    Time: 24 ms (74.28%), Space: 46.5 MB (55.66%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,24 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int currentPrefixSum = 0;
-        int answer = 0;
-        Map<Integer, Integer> prefixSumSeenCount = new HashMap<>();
-        prefixSumSeenCount.put(0, 1);
- 
-        // T: O(n)
-        // S: O(n) 
- 
-        for (int i = 0; i < nums.length; i++) {
-            currentPrefixSum += nums[i];
+    int n =nums.length;
+    int prefixSum = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0,1);
+    int count =0;
 
-            answer += prefixSumSeenCount.getOrDefault(currentPrefixSum - k, 0);
- 
-            // After we have processed current prefix, we should add 1 to its seenCount.
-            prefixSumSeenCount.put(
-                currentPrefixSum,
-                prefixSumSeenCount.getOrDefault(currentPrefixSum, 0) + 1);
+    for (int num : nums) {
+        prefixSum+=num;
+        if(map.containsKey(prefixSum - k)){
+        count +=map.get(prefixSum -k);
         }
-        
-        return answer;
+        map.put(prefixSum, map.getOrDefault(prefixSum, 0)+1);
     }
-}
+    
+    return count;}}
+    
+    
